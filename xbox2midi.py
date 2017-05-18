0bcd9ae1cf6edcb5cf59303eac5581cfab43cddc
Michael Biggs <eraserhead@gmail.com>
2017-05-18 16:49:04 -0600

    starting to implement inputs

--- xbox2midi.py
+++ xbox2midi.py
@@ -4,23 +4,47 @@ def convertInputToMidi(pad):
     # Left analog stick (-1 to 1)
     leftX = pad.leftX()
     if leftX != 0:
-        print("leftX: ", leftX, "\n");
+        print "leftX:", leftX
 
     leftY = pad.leftY()
+    if leftY != 0:
+        print "leftY:", leftY
 
     # Right analog stick (-1 to 1)
     rightX = pad.rightX()
-    rightY = pad.rightY()    
+    if rightX:
+        print "rightX:", rightX
+
+    rightY = pad.rightY()
+    if rightY:
+        print "rightY:", rightY
 
     # Dpad (0 or 1)
     dpadUp = pad.dpadUp()
+    if dpadUp:
+        print "dpad up"
+
     dpadDown = pad.dpadDown()
+    if dpadDown:
+        print "dpad down"
+
     dpadLeft = pad.dpadLeft()
+    if dpadLeft:
+        print "dpad left"
+
     dpadRight = pad.dpadRight()
+    if dpadRight:
+        print "dpad right"
 
     # Buttons (0 or 1)
     start = pad.Start()
+    if start:
+        print "start"
+
     leftStick = pad.leftThumbstick()
+    if leftStick:
+        print "leftStick"
+
     rightStick = pad.rightThumbstick()
     a = pad.A()
     b = pad.B()
@@ -33,18 +57,27 @@ def convertInputToMidi(pad):
     leftTrigger = pad.leftTrigger()
     rightTrigger = pad.rightTrigger()
 
+def printConnected(connected):
+    if connected:
+        print "Controller Connected"
+    else:
+        print "Controller Disconnected"
+
 if __name__ == "__main__":
     print "xbox2midi running: Press Back button to exit"
 
     pad = xbox.Joystick()
+    connected = pad.connected()
+    printConnected(connected)
 
     # Loop until back button is pressed
     while not pad.Back():
 
         # Show connection status
-        if not pad.connected():
-            print "Controller Disconnected"
-            break
+        wasConnected = connected
+        connected = pad.connected()
+        if wasConnected != connected:
+            printConnected(connected)
     
 	# Start reading input
 	convertInputToMidi(pad)
