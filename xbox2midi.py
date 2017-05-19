de8abca89a254e32049a4630c0b09f3b2b094f65
Michael Biggs <mbiggs@pixio.com>
2017-05-18 19:49:32 -0600

    tinkering

--- xbox2midi.py
+++ xbox2midi.py
@@ -12,7 +12,33 @@ class AnalogStick:
         self.description = description
         self.getValue = getValue
 
-def convertInputToMidi(buttons, joysticks):
+# define buttons
+dpadUp = Button("dpad up", "pad.dpadUp()")
+dpadDown = Button("dpad down", "pad.dpadDown()")
+dpadLeft = Button("dpad left", "pad.dpadLeft()")
+dpadRight = Button("dpad right", "pad.dpadRight()")
+start = Button("start", "pad.Start()")
+leftStick = Button("left stick button", "pad.leftThumbstick()")
+rightStick = Button("right stick button", "pad.rightThumbstick()")
+a = Button("a", "pad.A()")
+b = Button("b", "pad.B()")
+x = Button("x", "pad.X()")
+y = Button("y", "pad.Y()")
+leftBumper = Button("left bumper", "pad.leftBumper()")
+rightBumper = Button("right bumper", "pad.rightBumper()")
+
+# define analog joysticks
+leftX = AnalogStick("left X", "pad.leftX()")
+leftY = AnalogStick("left Y", "pad.leftY()")
+rightX = AnalogStick("right X", "pad.rightX()")
+rightY = AnalogStick("right Y", "pad.rightY()")
+leftTrigger = AnalogStick("left trigger", "pad.leftTrigger()")    
+rightTrigger = AnalogStick("right trigger", "pad.rightTrigger()")
+
+buttons = [dpadUp, dpadDown, dpadLeft, dpadRight, start, leftStick, rightStick, a, b, x, y, leftBumper, rightBumper]
+joysticks = [leftX, leftY, rightX, rightY, leftTrigger, rightTrigger]
+
+def convertInputToMidi():
     # handle buttons
     for button in buttons:    
         if not button.holding:
@@ -43,41 +69,14 @@ if __name__ == "__main__":
 
     # Loop until back button is pressed
     while not pad.Back():
-
         # Show connection status
         wasConnected = connected
         connected = pad.connected()
         if wasConnected != connected:
             printConnected(connected)
-    
-        # define buttons
-        dpadUp = Button("dpad up", "pad.dpadUp()")
-        dpadDown = Button("dpad down", "pad.dpadDown()")
-        dpadLeft = Button("dpad left", "pad.dpadLeft()")
-        dpadRight = Button("dpad right", "pad.dpadRight()")
-        start = Button("start", "pad.Start()")
-        leftStick = Button("left stick button", "pad.leftThumbstick()")
-        rightStick = Button("right stick button", "pad.rightThumbstick()")
-        a = Button("a", "pad.A()")
-        b = Button("b", "pad.B()")
-        x = Button("x", "pad.X()")
-        y = Button("y", "pad.Y()")
-        leftBumper = Button("left bumper", "pad.leftBumper()")
-        rightBumper = Button("right bumper", "pad.rightBumper()")
 
-        # define analog joysticks
-        leftX = AnalogStick("left X", "pad.leftX()")
-        leftY = AnalogStick("left Y", "pad.leftY()")
-        rightX = AnalogStick("right X", "pad.rightX()")
-        rightY = AnalogStick("right Y", "pad.rightY()")
-        leftTrigger = AnalogStick("left trigger", "pad.leftTrigger()")    
-        rightTrigger = AnalogStick("right trigger", "pad.rightTrigger()")
-
-
-        buttons = [dpadUp, dpadDown, dpadLeft, dpadRight, start, leftStick, rightStick, a, b, x, y, leftBumper, rightBumper]
-        joysticks = [leftX, leftY, rightX, rightY, leftTrigger, rightTrigger]
-
-	    convertInputToMidi(buttons, joysticks)
+        # run conversions on inputs
+        convertInputToMidi()
 
     # Close out when done
     pad.close()
